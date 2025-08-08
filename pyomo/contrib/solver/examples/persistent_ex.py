6898832db6e4a61671dad942574ddd2617a1d600
Michael Bynum <mlbynum@sandia.gov>
2025-08-07 22:26:00 -0600

    solver interface examples

---
+++ pyomo/contrib/solver/examples/persistent_ex.py
@@ -0,0 +1,25 @@
+import pyomo.environ as pyo
+from pyomo.contrib.solver.common.factory import SolverFactory
+from pyomo.common.timing import HierarchicalTimer
+
+
+def main():
+    m = pyo.ConcreteModel()
+    m.x = pyo.Var()
+    m.y = pyo.Var()
+    m.p = pyo.Param(mutable=True, initialize=0)
+    m.obj = pyo.Objective(expr=m.y)
+    m.c1 = pyo.Constraint(expr=m.y >= m.x + m.p)
+    m.c2 = pyo.Constraint(expr=m.y >= -m.x)
+
+    timer = HierarchicalTimer()
+    opt = SolverFactory('highs')
+    opt.config.timer = timer
+    for i in range(0, 100):
+        m.p.value = i
+        results = opt.solve(m)
+    print(opt.config.timer)
+
+
+if __name__ == '__main__':
+    main()
