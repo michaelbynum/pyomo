15bbf36ae820e71f07250b51f412f37c001cd0e5
Michael Bynum <mlbynum@sandia.gov>
2025-08-16 12:22:23 -0600

    rosenbrock example

--- pyomo/contrib/coramin/examples/rosenbrock.py
+++ pyomo/contrib/coramin/examples/rosenbrock.py
@@ -16,31 +16,31 @@ def create_nlp(a, b):
 
 def main():
     a = 1
-    b = 1
+    b = 100
     nlp = create_nlp(a, b)
     rel = coramin.relaxations.relax(nlp)
+    rel.pprint()
 
     nlp_opt = SolverFactory('ipopt')
-    rel_opt = SolverFactory('gurobi_persistent')
+    rel_opt = SolverFactory('highs')
 
     res = nlp_opt.solve(nlp, tee=False)
     ub = res.incumbent_objective
+    print(f'upper bound: {ub};    x: {nlp.x.value};    y: {nlp.y.value}')
 
     res = rel_opt.solve(rel, tee=False)
     lb = res.objective_bound
+    print(f'lower bound: {lb};    x: {nlp.x.value};    y: {nlp.y.value}')
 
-    print('lb: ', lb)
-    print('ub: ', ub)
+    for iter_ndx in range(30):
+        for r in coramin.relaxations.relaxation_data_objects(rel, descend_into=True, active=True):
+            v = r.get_rhs_vars()[0]
+            r.add_oa_point(var_values=(v.value,))
+            r.rebuild()
 
-    print('nlp results:')
-    print('--------------------------')
-    nlp.x.pprint()
-    nlp.y.pprint()
-
-    print('relaxation results:')
-    print('--------------------------')
-    rel.x.pprint()
-    rel.y.pprint()
+        res = rel_opt.solve(rel, tee=False)
+        lb = res.objective_bound
+        print(f'iter: {iter_ndx};    lower bound: {lb};    x: {nlp.x.value};    y: {nlp.y.value}')
 
 
 if __name__ == '__main__':
